43af9b02109165c5e7dd94bfd89fe2cb0f9e26ac
liutao <594963425@qq.com>
2020-09-17 18:02:46 +0800

    aidl访问 作为服务端...........

--- app/src/main/java/com/example/ktapp/aidls/CalcService.java
+++ app/src/main/java/com/example/ktapp/aidls/CalcService.java
@@ -14,10 +14,13 @@ import com.example.ktapp.data.db.UserDatabase;
  * @ProjectName: KtApp
  * @Package: com.example.ktapp.aidl
  * @ClassName: CalcService
- * @Description: AIDL用到的不能进行混淆
+ * @Description: Service的作用其实就是为我们创建Binder驱动，即服务端与客户端连接的桥梁。  AIDL用到的不能进行混淆
  * @Author: LiuTao
  * @CreateDate: 2020/9/17 8:38
  * @UpdateUser: LiuTao
+ *
+ * AIDL其实通过我们写的aidl文件，帮助我们生成了一个接口，一个Stub类用于服务端，一个Proxy类用于客户端调用。
+ * 那么我们是否可以不通过写AIDL来实现远程的通信呢？下面向大家展示如何完全不依赖AIDL来实现客户端与服务端的通信。
  */
 public class CalcService extends Service {
     private static final String TAG = "server";
